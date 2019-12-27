622b581cc685a88f60e110b017b31fcc573e194b
sisi28888 <sy2882@columbia.edu>
2019-12-27 23:03:46 +0000

    map

---
+++ squirrel/map/views.py
@@ -0,0 +1,13 @@
+
+Learn more or give us feedback
+from django.shortcuts import render
+# Create your views here
+
+
+from .models import squirrels
+
+
+def squirrels_map(request):
+    sightings=squirrels.objects.all()[:45]
+    
+    return render(request, 'map/map.html',{'sightings':sightings})
