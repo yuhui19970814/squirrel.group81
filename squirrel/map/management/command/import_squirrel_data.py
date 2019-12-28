627681b3a89ae2bcda507632f3957db10afdf2cf
sisi28888 <sy2882@columbia.edu>
2019-12-28 01:51:32 +0000

    map

---
+++ squirrel/map/management/command/import_squirrel_data.py
@@ -0,0 +1,49 @@
+
+Learn more or give us feedback
+from map.models import squirrels
+
+import csv
+from django.core.management import BaseCommand
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument('csv_file')
+
+    def handle(self,*args,**kargs):
+        path=kargs['csv_file']
+        with open(path) as f:
+            reader = csv.DictReader(f)
+            data=list(reader)
+            for row in data:
+                try:
+                    p=squirrels(
+                        Unique_Squirrel_ID=row['Unique Squirrel ID'],
+                        Longitude=row['X'],
+                        Latitude=row['Y'],
+                        Shift=row['Shift'],
+                        Date=row['Date'][4:]+'-'+row['Date'][:2]+'-'+row['Date'][2:4],
+                        Age=row['Age'],
+                        Primary_fur_color=row['Primary Fur Color'],
+                        Location=row['Location'],
+                        Specific_location=row['Specific Location'],
+                        Running=row['Running'].capitalize(),
+                        Chasing=row['Chasing'].capitalize(),
+                        Climbing=row['Climbing'].capitalize(),
+                        Eating=row['Eating'].capitalize(),
+                        Foraging=row['Foraging'].capitalize(),
+                        Other_Activities=row['Other Activities'],
+                        Kuks=row['Kuks'].capitalize(),
+                        Quaas=row['Quaas'].capitalize(),
+                        Moans=row['Moans'].capitalize(),
+                        Tail_flags=row['Tail flags'].capitalize(),
+                        Tail_twitches=row['Tail twitches'].capitalize(),
+                        Approaches=row['Approaches'].capitalize(),
+                        Indifferent=row['Indifferent'].capitalize(),
+                        Runs_from=row['Runs from'].capitalize(),
+
+                    
+                        )
+                    p.save()
+                except:
+                    pass
